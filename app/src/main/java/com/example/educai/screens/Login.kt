2798376dc3e4c49dd8f7fa70278a3ade1da5e774
ribgu <gustavo.ribeiro@sptech.school>
2024-09-09 11:05:10 -0300

    remove unused imports

--- app/src/main/java/com/example/educai/screens/Login.kt
+++ app/src/main/java/com/example/educai/screens/Login.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.educai.R
 import com.example.educai.components.LoginContainer
 import com.example.educai.ui.theme.MediumPurple
