718e8087ed0278eb5ccc828f20001fcea76cf3f2
ribgu <gustavo.ribeiro@sptech.school>
2024-11-03 18:45:00 -0300

    remove comentário

--- app/src/main/java/com/example/educai/data/services/PostService.kt
+++ app/src/main/java/com/example/educai/data/services/PostService.kt
@@ -12,36 +12,3 @@ interface PostService {
     @GET("classroom/{classroomId}/posts")
     suspend fun getPostsByClassroom(@Path("classroomId") classroomId: String): List<Post>
 }
-
-//async createPost(body: { title: string, description: string, datePosting: string, classroomId: string }, file: File): Promise<PostType> {
-//    const formData = new FormData()
-//    formData.append('title', body.title)
-//    formData.append('description', body.description)
-//    formData.append('datePosting', body.datePosting)
-//    formData.append('classroomId', body.classroomId)
-//    formData.append('file', file)
-//
-//    const response = await this.axios.post('/posts', formData)
-//    const post: PostType = response.data
-//            return  post
-//}
-//
-//async getUrlArquivoPost(postId: string): Promise<string> {
-//    return (await this.axios.get(`/posts/${postId}/download`)).data
-//}
-//
-//async deletePost(postId: string): Promise<void> {
-//    return (await this.axios.delete(`/posts/${postId}`))
-//}
-//
-//async updatePost(postId: string, body: { title?: string, description?: string }): Promise<void> {
-//    return (await this.axios.patch(`/posts/${postId}`, body))
-//}
-//
-//async getPostsByClassroom(classroomId: string): Promise<PostType[]> {
-//    return (await this.axios.get(`/classroom/${classroomId}/posts`)).data
-//}
-//
-//async getClassworkById(classworkId: string): Promise<Classwork> {
-//    return (await this.axios.get(`/classwork/${classworkId}`)).data
-//}
